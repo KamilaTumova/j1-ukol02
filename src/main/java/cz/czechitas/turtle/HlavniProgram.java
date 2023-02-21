f26c1525f4b341f90fbca5c999c797a0f0d14c67
Kamila <kaime31@gmail.com>
2023-02-21 20:55:53 +0100

    obdelnik+ctverec

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -7,10 +7,33 @@ public class HlavniProgram {
 
     public void start() {
         zofka = new Turtle();
+
+        //nakresliCtverec(20);
+        //nakresliObdelnik(20,40);
+
     }
 
-    public static void main(String[] args) {
-        new HlavniProgram().start();
+
+public void nakresliCtverec(double strana_a){
+    for (int i = 0; i < 4; i++) {
+        zofka.move(strana_a);
+        zofka.turnRight(90);
     }
 
 }
+
+public void nakresliObdelnik(double strana_a, double strana_b){
+    for (int i = 0; i < 2; i++) {
+        zofka.move(strana_a);
+        zofka.turnRight(90);
+        zofka.move(strana_b);
+        zofka.turnRight(90);
+}
+    }
+    public static void main(String[] args) {
+        new HlavniProgram().start();
+
+    }
+
+}
+
